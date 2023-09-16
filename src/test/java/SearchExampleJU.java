cc24cbb9f1a57efa32fc0611cff1bc3b1c69fefb
Julia Menschikova <yu.menschikowa2015@yandex.ru>
2023-09-16 19:04:23 +0300

    Доработан тест + дан ответ на вопрос

--- src/test/java/SearchExampleJU.java
+++ src/test/java/SearchExampleJU.java
@@ -2,31 +2,63 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchExampleJU {
 
     @BeforeAll
-     static void BeforeAll() {
+    static void beforeAll() {
         Configuration.baseUrl = "https://github.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
-    void SearchJUnit() {
+    void searchJUnit() {
         // 1 Откройте страницу Selenide в Github
         open("/selenide/selenide");
+
         // 2 Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+
         // 3 Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-box").$(withText("Show")).click();
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+
         // 4 Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
 
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
+        $(".markdown-body").shouldHave(text("class Tests {\n" +
+                "  @RegisterExtension \n" +
+                "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();\n" +
+                "\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
 
     }
-
-
 
 
 }
